7ed285d5e4a96dbdcd1499cd6fea6329363c9c5a
Andrey Bogatyrev <abogatyrev@gmail.com>
2022-10-11 17:26:17 +0300

    - wildfly updates

--- wildfly-server/wildfly-standalone/scripts/postInstall.sh
+++ wildfly-server/wildfly-standalone/scripts/postInstall.sh
@@ -4,3 +4,5 @@ chmod +x /opt/wildfly/bin/*.sh
 
 # remove all Windows end of line symbols
 sed -i 's/\r//g' /opt/wildfly/bin/*.sh
+sed -i 's/\r//g' /opt/wildfly/bin/*.conf
+sed -i 's/\r//g' /opt/wildfly/bin/*.xml
